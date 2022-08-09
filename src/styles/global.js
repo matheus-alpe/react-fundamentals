b6d01e14063a10e31165251ec65ad29cb4b569af
Matheus Alves Pereira <matheus.pereira@dotgroup.com.br>
2022-08-08 23:02:47 -0300

    feat: theme provider styled-components

--- src/styles/global.js
+++ src/styles/global.js
@@ -2,8 +2,8 @@ import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
   body {
-    background-color: #222;
+    background-color: ${props => props.theme.backgroundColor};
     font-family: sans-serif;
-    color: #fff;
+    color: ${props => props.theme.textColor};
   }
 `
