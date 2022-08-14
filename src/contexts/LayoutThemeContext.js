b56144c9b4ff436c260a90d3f9fe78706b772d2f
Matheus Alves Pereira <matttalves@gmail.com>
2022-08-14 20:24:55 -0300

    feat: class component state

--- src/contexts/LayoutThemeContext.js
+++ src/contexts/LayoutThemeContext.js
@@ -1,53 +1,44 @@
-import React, { useState, createContext, useMemo, useEffect, useRef } from 'react'
+import React, { createContext, Component } from 'react'
 import { ThemeProvider } from 'styled-components'
 
 import themes from '../styles/themes'
 
 export const ThemeContext = createContext()
 
-export function LayoutThemeProvider(props) {
-  const [theme, setTheme] = useState('dark')
+export class LayoutThemeProvider extends Component {
+  constructor(props) {
+    super(props)
 
-  const currentTheme = useMemo(() => {
-    return themes[theme] || themes.dark
-  }, [theme])
+    this.state = {
+      theme: window.localStorage.getItem('theme') || 'dark'
+    }
+  }
 
-  function handleToggleTheme() {
-    setTheme((prevThemeState) => {
-      const theme = prevThemeState === 'dark' ? 'light' : 'dark'
+  handleToggleTheme() {
+    this.setState(prevState => {
+      const theme = (prevState.theme === 'dark' ? 'light' : 'dark')
       window.localStorage.setItem('theme', theme)
-      return theme
+
+      return {
+        theme
+      }
     })
   }
 
-  // hooks
-  useEffect(() => {
-    const theme = window.localStorage.getItem('theme')
-    if (!theme) return
-    setTheme(theme)
-  }, [])
+  render() {
+    const { theme } = this.state
 
-  //not execute on first render
-  const isFirstRender = useRef(true)
-  useEffect(() => {
-    if (isFirstRender.current) {
-      isFirstRender.current = false
-      return
-    }
-    console.debug('useEffect after first render')
-  }, [theme])
-
-  return (
-    <ThemeProvider theme={currentTheme}>
-      <ThemeContext.Provider
-        value={{
-          theme,
-          onToggleTheme: handleToggleTheme,
-        }}
-      >
-        {props.children}
-      </ThemeContext.Provider>
-    </ThemeProvider>
-
-  )
+    return (
+      <ThemeProvider theme={themes[theme] || themes.dark}>
+        <ThemeContext.Provider
+          value={{
+            theme,
+            onToggleTheme: this.handleToggleTheme.bind(this),
+          }}
+        >
+          {this.props.children}
+        </ThemeContext.Provider>
+      </ThemeProvider>
+    )
+  }
 }
