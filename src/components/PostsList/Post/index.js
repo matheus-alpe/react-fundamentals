f73560815591c2b0b854d980acc1e3c238248b22
Matheus Alves Pereira <matttalves@gmail.com>
2022-08-15 22:44:07 -0300

    feat: class component proptypes

--- src/components/PostsList/Post/index.js
+++ src/components/PostsList/Post/index.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 
 import { Container } from './styles';
 
 export default class Post extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  }
+
   render() {
     const { title, description } = this.props
 
