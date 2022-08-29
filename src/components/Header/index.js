fcd442aceb356f48e5319dd46b1b5cd2a1b7365a
Matheus Alves Pereira <matttalves@gmail.com>
2022-08-29 19:42:19 -0300

    feat: router push navigation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,25 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import { Container } from './styles'
 
 import ThemeSwitcher from '../ThemeSwitcher'
 import { Link } from 'react-router-dom'
 
 export default function Header() {
+  const history = useHistory()
+  console.log(history)
+
+  function handleNavigate() {
+    history.push('/posts')
+  }
+
   return (
     <Container>
       <h1>
         <Link to="/">JStack's Blog</Link>
       </h1>
+
+      <button onClick={handleNavigate} style={{ color: 'red' }}>Posts</button>
 
       <ThemeSwitcher />
     </Container>
